8926fb05a7fc3708df6a22a05bd48cf5d62d34b3
Mykhailo Syritchenko <syritchenko@gmail.com>
2022-01-26 23:03:42 +0100

    Modified src/store/customerReducer.jsx

--- src/store/customerReducer.jsx
+++ src/store/customerReducer.jsx
@@ -1,10 +1,33 @@
+// const defaultState = {
+// 	customers: []
+// };
+
+const ADD_CUSTOMER = 'ADD_CUSTOMER';
+const ADD_MANY_CUSTOMERS = 'ADD_MANY_CUSTOMERS';
+const REMOVE_CUSTOMERS = 'REMOVE_CUSTOMERS';
+
 export const customerReducer = (state = [], action) => {
 	switch (action.type) {
-		case 'ADD_CUSTOMER':
-			return { ...state, customers: [...state.customers, action.payload] };
-		case 'REMO_CUSTOMER':
-			return { ...state, cash: state.cash - action.payload };
+		case ADD_MANY_CUSTOMERS:
+			return [...state, ...action.payload];
+
+		case ADD_CUSTOMER:
+			// return { ...state, customers: [...state.customers, action.payload] };
+			return [...state, action.payload];
+		case REMOVE_CUSTOMERS:
+			// return {...state, customers: state.customers.filter(customer => customer.id !== action.payload)}
+			return state.filter((customer) => customer.id !== action.payload);
 		default:
 			return state;
 	}
 };
+
+export const addManyCustomersAction = (payload) => ({
+	type: ADD_MANY_CUSTOMERS,
+	payload
+});
+export const addCustomerAction = (payload) => ({ type: ADD_CUSTOMER, payload });
+export const removeCustomerAction = (payload) => ({
+	type: REMOVE_CUSTOMERS,
+	payload
+});
